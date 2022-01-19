6ac3cf345bb132e1626d1c7a4c53721e0f851e10
lintoki <lilinchong@100tal.com>
2022-01-19 18:07:05 +0800

    add

--- algo/linklist.go
+++ algo/linklist.go
@@ -1,21 +1,23 @@
 package main
 
+import "fmt"
+
 type link struct {
 	data int
 	next *link
 	len  int
 }
 
-func (l *link) new() *link{
+func (l *link) new(data int) *link {
 	head := new(link)
-	head.data = 1
+	head.data = data
 	head.next = nil
 	head.len = 1
 	return head
 }
 
 func (l *link) add(data int) {
-	newLink := new(link)
+	newLink := l.new(data)
 	newLink.next = l
 
 	if l.next == nil {
@@ -26,16 +28,51 @@ func (l *link) add(data int) {
 
 	cur := l
 	for {
-		if cur.next == cur {
+		if cur.next == l {
 			cur.next = newLink
 			l.len++
+			return
 		}
 
 		cur = cur.next
 	}
 }
 
+func (l *link) list() {
+	tmp := l
+	for {
+		fmt.Println(tmp.data)
+		if tmp.next == l {
+			return
+		}
+
+		tmp = tmp.next
+	}
+}
+
 func main() {
-	h := new(link).new()
-	
+	h := new(link).new(1)
+	for i := 2; i < 6; i++ {
+		h.add(i)
+	}
+
+	h.list()
+
+	cur := h
+	i := 1
+	for {
+		if cur.next == cur {
+			fmt.Println(cur.data)
+			return
+		}
+
+		if i == 2 {
+			i = 1
+			cur.next = cur.next.next
+		} else {
+			i++
+		}
+
+		cur = cur.next
+	}
 }
