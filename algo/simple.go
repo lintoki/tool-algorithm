21880a4343eb88a46d64dbd4cf664139293efcc3
lintoki <1097844930@qq.com>
2022-02-18 20:13:26 +0800

    1

--- algo/simple.go
+++ algo/simple.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //斐波那契额数列
 func fib(n int) {
@@ -29,8 +31,114 @@ func fibcourson(n int) int {
 	return fibcourson(n-1) + fibcourson(n-2)
 }
 
+//继承
+type dad struct {
+	name string
+	age  string
+}
+
+type son struct {
+	dad
+	like string
+}
+
+func (d dad) say() {
+	fmt.Println("i am  dad")
+}
+
+//接口
+type animal interface {
+	Eat()
+	Run()
+}
+
+type cat struct {
+	Age    int
+	Weight int
+}
+
+type dog struct {
+	Age int
+}
+
+func (c cat) Eat() {
+	fmt.Println("cat eat la")
+}
+
+func (c cat) Run() {
+	fmt.Println("cat run")
+}
+
+func (d dog) Eat() {
+	fmt.Println("cat eat la")
+}
+
+func (d dog) Run() {
+	fmt.Println("cat run")
+}
+
+func playPet(a animal) {
+	a.Eat()
+	a.Run()
+}
+
+//自定义类型
+type expr interface {
+	eval(env Env) float64
+}
+type va string
+type ligle float64
+type binary struct {
+	op   rune
+	x, y expr
+}
+type Env map[va]float64
+
+func (b binary) eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.eval(env) + b.y.eval(env)
+	case '-':
+		return b.x.eval(env) - b.y.eval(env)
+	default:
+		return b.x.eval(env) + b.y.eval(env)
+	}
+}
+
+func (l ligle) eval(env Env) float64 {
+	return float64(l)
+}
+
+//类型断言，判断s是否在有s.run方法
+func judgeinterface(s interface{}) {
+	type a interface {
+		run()
+	}
+
+	if s, ok := s.(a); ok {
+		s.run()
+	}
+
+	return
+}
+
 func main() {
+	//断言
+	var x interface{} = 1
+	if f, ok := x.(int); ok {
+		fmt.Println(f)
+	}
+
 	//fib2(5)
-	var i int8 = 127
-	fmt.Println(i,i+1,i+11,i*i)
+	//var i int8 = 127
+	//fmt.Println(i,i+1,i+11,i*i)
+
+	//s:= son{dad{"11","22"},"22"}
+	//a := new(son)
+	//a.say()
+
+	//接口
+	//c := cat{1, 1}
+	//playPet(c)
+
 }
